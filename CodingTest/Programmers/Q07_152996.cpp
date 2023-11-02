e7fb594c3a3e236bbb61dbb76e92a5780b654f0d
youns119 <youns119@naver.com>
2023-11-03 03:13:16 +0900

    asdf

--- CodingTest/Programmers/Q07_152996.cpp
+++ CodingTest/Programmers/Q07_152996.cpp
@@ -103,35 +103,26 @@ void Input(int N, vector<int>& weights)
 
 // 가장 획기적이고 깔끔한 풀이. 생각도 못한 풀이라 보고 공부가 많이 됐다.
 long long solution(vector<int> weights) {
-    long long answer = 0;
 
+    long long answer = 0;
     vector<long long> arr(2001, 0);
 
     for (const auto v : weights)
-    {
         arr[v]++;
-    }
 
     for (int i = 100; i <= 1000; ++i)
     {
         if (arr[i] == 0)
-        {
             continue;
-        }
 
         answer += arr[i] * (arr[i] - 1) / 2;
-
         answer += arr[i] * arr[2 * i];
 
         if ((i * 3) % 2 == 0)
-        {
             answer += arr[i] * arr[i * 3 / 2];
-        }
 
         if ((i * 4) % 3 == 0)
-        {
             answer += arr[i] * arr[i * 4 / 3];
-        }
     }
 
     return answer;
