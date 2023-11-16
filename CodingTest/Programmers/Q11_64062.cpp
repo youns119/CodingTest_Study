61c07fde80a56cdd5a182f5dc3896d9261230a27
youns119 <youns119@naver.com>
2023-11-16 23:46:52 +0900

    asdf

--- CodingTest/Programmers/Q11_64062.cpp
+++ CodingTest/Programmers/Q11_64062.cpp
@@ -63,3 +63,38 @@ int solution(vector<int> stones, int k)
 
 	return L;
 }
+
+// 처음에 개인적으로 해보려고 시도했던 접근법. 
+// 슬라이딩 윈도우로 구현했으며 속도도 이진탐색과 차이가 나지 않을 정도로 빠르다
+//int solution(vector<int> stones, int k)
+//{
+//	int answer = 987654321;
+//	deque<pair<int, int>> DQ;
+//
+//	for (int i = 0; i < stones.size(); i++)
+//	{
+//		while (DQ.empty() == false && DQ.front().second < i - k + 1) DQ.pop_front();
+//		while (DQ.empty() == false && DQ.back().first < stones[i]) DQ.pop_back();
+//		DQ.push_back(make_pair(stones[i], i));
+//		if (i >= k - 1 && DQ.front().first < answer) answer = DQ.front().first;
+//	}
+//	return answer;
+//}
+
+// 멀티셋을 활용한 풀이. 
+// 코드는 굉장히 단순해지지만 이진 탐색에 비해 효율성검사에서 속도가 몇십배는 차이가 난다.
+//multiset <int> s;
+//
+//int solution(vector<int> a, int k) {
+//	int ans = 1e9;
+//	for (int i = 0; i < k - 1; i++) {
+//		s.insert(a[i]);
+//	}
+//	for (int i = k - 1; i < a.size(); i++) {
+//		s.insert(a[i]);
+//		auto it = s.end(); --it;
+//		ans = min(ans, *it);
+//		s.erase(s.find(a[i - (k - 1)]));
+//	}
+//	return ans;
+//}
