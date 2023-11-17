01ce3ef887708769fd8fe0e7a1b619c951a1ec09
youns119 <youns119@naver.com>
2023-11-17 20:23:28 +0900

    asdf

--- CodingTest/Programmers/Q12_77885.cpp
+++ CodingTest/Programmers/Q12_77885.cpp
@@ -1,15 +1,18 @@
 // https://yabmoons.tistory.com/632
 #include <iostream>
 #include <vector>
+#include <bitset>
 
 using namespace std;
 
-vector<long long> solution(vector<long long> numbers);
+typedef long long ll;
+
+vector<ll> solution(vector<ll> numbers);
 
 int main()
 {
 	int N;
-	vector<long long> numbers;
+	vector<ll> numbers;
 
 	cin >> N;
 	numbers.assign(N, 0);
@@ -25,20 +28,68 @@ int main()
 	return 0;
 }
 
-vector<long long> solution(vector<long long> numbers)
+vector<ll> solution(vector<ll> numbers)
 {
-	vector<long long> answer;
+	vector<ll> answer;
 
-	for (int i = 0; i < numbers.size(); i++)
+	for (auto number : numbers)
 	{
-		if (numbers[i] % 2 == 0) answer.push_back(numbers[i] + 1);
+		if (number % 2 == 0) answer.push_back(number + 1);
 		else
 		{
+			bitset<50> bs(number);
+			int num;
 
+			for (int i = 0; i < bs.size(); i++)
+			{
+				if (bs[i] == 0)
+				{
+					bs[i] = 1;
+					num = i;
+					break;
+				}
+			}
+
+			for (int i = num - 1; i >= 0; i--)
+			{
+				if (bs[i] == 1)
+				{
+					bs[i] = 0;
+					break;
+				}
+			}
+
+			answer.push_back(bs.to_ullong());
 		}
-
 	}
-
 
 	return answer;
 }
+
+// 속도는 내가 위의 풀이와 비슷
+// 하지만 코드가 더 간결하다
+//vector<long long> solution(vector<long long> numbers) 
+//{
+//	vector<long long> answer;
+//
+//	for (long long number : numbers)
+//	{
+//		long long bit = 1;
+//		while ((number & bit) > 0) bit <<= 1;
+//		answer.push_back(number + bit - (bit >> 1));
+//	}
+//
+//	return answer;
+//}
+
+// 가장 신박한 방법
+// 어떻게 생각해 냈는지도 모르겠고 이해하기도 힘들다...
+//vector<long long> solution(vector<long long> numbers) {
+//	vector<long long> answer;
+//	for (ll& n : numbers) {
+//		ll idx = (~n & -~n);
+//		ll ans = n + idx - (idx >> 1);
+//		answer.push_back(ans);
+//	}
+//	return answer;
+//}
