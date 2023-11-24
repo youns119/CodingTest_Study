6245483ea3dcbf6e93cfdda19b944338258af3c2
youns119 <youns119@naver.com>
2023-11-24 22:35:49 +0900

    asdf

---
+++ CodingTest/Programmers/Q13_42892.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> nodeinfo);
+
+int main()
+{
+
+	return 0;
+}
+
+vector<vector<int>> solution(vector<vector<int>> nodeinfo)
+{
+	vector<vector<int>> answer;
+
+
+
+	return answer;
+}
