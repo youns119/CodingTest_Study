eab78c5d60fdec8d5c6392499c345e995e91d49f
youns119 <youns119@naver.com>
2023-11-25 00:15:11 +0900

    asdf

--- CodingTest/Programmers/Q13_42892.cpp
+++ CodingTest/Programmers/Q13_42892.cpp
@@ -7,6 +7,24 @@ vector<vector<int>> solution(vector<vector<int>> nodeinfo);
 
 int main()
 {
+	int N;
+	vector<vector<int>> nodeinfo;
+	vector<vector<int>> results;
+
+	cin >> N;
+	nodeinfo.assign(N, vector<int>(2, 0));
+
+	for (int i = 0; i < N; i++)
+		cin >> nodeinfo[i][0] >> nodeinfo[i][1];
+
+	results = solution(nodeinfo);
+	for (auto result : results)
+	{
+		for (auto r : result)
+			cout << r << " ";
+
+		cout << "\n";
+	}
 
 	return 0;
 }
