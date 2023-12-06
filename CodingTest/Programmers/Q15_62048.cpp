37aaf9dc8cb71c4308f041ec72fd4849166fa6b8
youns119 <youns119@naver.com>
2023-12-06 19:31:00 +0900

    asdf

---
+++ CodingTest/Programmers/Q15_62048.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+long long solution(int w, int h);
+
+int main()
+{
+	int w, h;
+	long long result;
+
+	cin >> w >> h;
+	result = solution(w, h);
+	cout << result;
+
+	return 0;
+}
+
+long long solution(int w, int h)
+{
+	long long answer = 0;
+
+
+
+	return answer;
+}
