c0cbda2f058b6255f92e64406235438e9fb8971a
youns119 <youns119@naver.com>
2025-05-19 03:43:04 +0900

    ㅁㄴㅇㄹ

--- Code/Programmers/Programmers_176962.cpp
+++ Code/Programmers/Programmers_176962.cpp
@@ -14,7 +14,7 @@ struct Task
 	string strName{};
 
 	int iStart{};
-	int iTime{};
+	int iDuration{};
 };
 
 int main()
@@ -23,7 +23,7 @@ int main()
 	cin.tie(nullptr);
 
 	int iNum{};
-	string strName{}, strStart{}, strTime{};
+	string strName{}, strStart{}, strDuration{};
 	vector<vector<string>> vecPlan;
 
 	cin >> iNum;
@@ -31,10 +31,10 @@ int main()
 
 	for (int i = 0; i < iNum; i++)
 	{
-		cin >> strName >> strStart >> strTime;
+		cin >> strName >> strStart >> strDuration;
 		vecPlan[i].push_back(strName);
 		vecPlan[i].push_back(strStart);
-		vecPlan[i].push_back(strTime);
+		vecPlan[i].push_back(strDuration);
 	}
 
 	vector<string> vecResult = solution(vecPlan);
@@ -69,37 +69,93 @@ vector<string> solution(vector<vector<string>> plans)
 		return tA.iStart < tB.iStart;
 		});
 
-	for (int i = 0; i < vecTask.size(); i++)
+	int iCurrTime{};
+	for (auto tTask : vecTask)
 	{
-		Task tCurrTask = vecTask[i];
+		int iDist = tTask.iStart - iCurrTime;
 
-		int iCurrEndTime = tCurrTask.iStart + tCurrTask.iTime;
-		int iNextStartTime = vecTask[i + 1].iStart;
-
-
-	}
-
-	while (!stTask.empty() && stTask.size() != 1)
-	{
-		Task tCurrTask = stTask.top();
-		stTask.pop();
-		Task tNextTask = stTask.top();
-
-		int iCurrEndTime = tCurrTask.iStart + tCurrTask.iTime;
-		int iNextStartTime = tNextTask.iStart;
-
-		if (iCurrEndTime > iNextStartTime)
+		while (!stTask.empty() && iDist > 0)
 		{
-			tCurrTask.iTime = iNextStartTime - tCurrTask.iStart;
-			tCurrTask.iStart = iNextStartTime;
+			Task tLastTask = stTask.top();
+			tLastTask.iDuration -= iDist;
+			stTask.pop();
 
-			stTask.push(tCurrTask);
+			iDist = -1 * tLastTask.iDuration;
+
+			if (tLastTask.iDuration <= 0)
+				answer.push_back(tLastTask.strName);
+			else stTask.push(tLastTask);
 		}
-		else
-			answer.push_back(tCurrTask.strName);
+
+		iCurrTime = tTask.iStart;
+		stTask.push(tTask);
 	}
 
-	answer.push_back(stTask.top().strName);
+	while (!stTask.empty())
+	{
+		answer.push_back(stTask.top().strName);
+		stTask.pop();
+	}
 
 	return answer;
 }
+
+// unordered_map, vector, stack 모두 사용하는 풀이
+// 와... 봐도 잘 모르겠다... 문제 자체가 너무 헷갈림...
+// 특이점이라면 시간을 1씩 계속 증가시키는 방법을 사용한다는 것
+
+#ifdef _RELEASE
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+bool comp(vector<string> a, vector<string> b) {
+	return a[1] < b[1];
+}
+
+int getTime(string s) {
+	int h = stoi(s.substr(0, 2));
+	int m = stoi(s.substr(3));
+	return h * 60 + m;
+}
+
+vector<string> solution(vector<vector<string>> plans) {
+	sort(plans.begin(), plans.end(), comp);
+
+
+	unordered_map<string, int> m;
+	for (vector<string> plan : plans) m[plan[0]] = stoi(plan[2]);
+
+	stack<vector<string>> st;
+	st.push(plans[0]);
+
+	int idx = 1;
+	int time = getTime(plans[0][1]);
+
+	vector<string> answer;
+	while (!st.empty()) {
+		time++;
+		string sub = st.top()[0];
+		m[sub]--;
+
+		if (m[sub] == 0) {
+			st.pop();
+			answer.push_back(sub);
+		}
+
+		if (idx < plans.size() && (time == getTime(plans[idx][1]) || st.empty())) {
+			st.push(plans[idx]);
+			time = getTime(plans[idx][1]);
+			idx++;
+		}
+	}
+	return answer;
+}
+
+#endif
