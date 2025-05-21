891f729f9a93e83ca1626e8f9f6d912e12c75534
youns119 <youns119@naver.com>
2025-05-21 18:47:06 +0900

    asdf

--- Code/BaekJoon/BaekJoon_2559.cpp
+++ Code/BaekJoon/BaekJoon_2559.cpp
@@ -39,3 +39,41 @@ int main()
 
 	return 0;
 }
+
+#ifdef _RELEASE
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N, K;
+	cin >> N >> K;
+
+	vector<int> temp(N);
+	for (int i = 0; i < N; ++i)
+		cin >> temp[i];
+
+	int sum = 0;
+	for (int i = 0; i < K; ++i)
+		sum += temp[i];
+
+	int maxSum = sum;
+
+	for (int i = K; i < N; ++i) {
+		sum += temp[i] - temp[i - K];
+		maxSum = max(maxSum, sum);
+	}
+
+	cout << maxSum << "\n";
+
+	return 0;
+}
+
+#endif
