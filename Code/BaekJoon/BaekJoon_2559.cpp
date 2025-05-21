88b34e66c7b883c9b22ce6aafe3540d5e5d0c163
youns119 <youns119@naver.com>
2025-05-21 17:50:59 +0900

    asdf

---
+++ Code/BaekJoon/BaekJoon_2559.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int iDay{}, iCons{};
+
+	cin >> iDay >> iCons;
+	vector<int> vecDay(iDay);
+	
+	vector<int> vecSum;
+	int iSum{};
+
+	for (int i = 0; i < iDay; i++)
+	{
+		cin >> vecDay[i];
+		iSum += vecDay[i];
+
+		if (i >= iCons)
+		{
+			iSum -= vecDay[i - iCons];
+			vecSum.push_back(iSum);
+		}
+	}
+
+	sort(vecSum.begin(), vecSum.end(), [](const int& iA, const int& iB)
+		{
+			return iA > iB;
+		}
+	);
+
+	cout << vecSum[0];
+
+	return 0;
+}
