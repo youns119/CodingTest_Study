0ca749d0a43d60aeb1116c094d9d710a048040e2
youns119 <youns119@naver.com>
2025-05-21 17:06:22 +0900

    ㅁㄴㅇㄹ

--- Code/Programmers/Programmers_340211.cpp
+++ Code/Programmers/Programmers_340211.cpp
@@ -72,3 +72,59 @@ int solution(vector<vector<int>> points, vector<vector<int>> routes)
 
     return answer;
 }
+
+// map + pair 방식으로 푸는 방법
+// 위의 풀이와 별 다른 점은 없다
+// 근데 map 컨테이너 순회하는 방식 좀 특이함... 저렇게도 되는구나..
+// 찾아보니까 구조적 바인딩이라는 기법이고, C++17 부터 되는 문법이라네 뭐야 ㅅㅂ
+
+#ifdef _RELEASE
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int solution(vector<vector<int>> points, vector<vector<int>> routes) {
+    int answer = 0;
+
+    map<int, map<pair<int, int>, int>> timePositions;
+
+    for (const auto& route : routes) {
+        int time = 0;
+
+        int r = points[route[0] - 1][0];
+        int c = points[route[0] - 1][1];
+
+        timePositions[time][{r, c}]++;
+
+        for (int i = 1; i < route.size(); ++i) {
+            int nr = points[route[i] - 1][0];
+            int nc = points[route[i] - 1][1];
+
+            while (r != nr) {
+                time++;
+                r += (nr > r) ? 1 : -1;
+                timePositions[time][{r, c}]++;
+            }
+
+            while (c != nc) {
+                time++;
+                c += (nc > c) ? 1 : -1;
+                timePositions[time][{r, c}]++;
+            }
+        }
+    }
+
+    for (const auto& [time, posMap] : timePositions) {
+        for (const auto& [pos, count] : posMap) {
+            if (count > 1)
+                answer++;
+        }
+    }
+
+    return answer;
+}
+
+#endif
