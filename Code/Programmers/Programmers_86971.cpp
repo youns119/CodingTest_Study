48ebe318248b314f622b71abdb2aad1e0656f5ab
youns119 <youns119@naver.com>
2025-05-26 18:40:39 +0900

    asdf

--- Code/Programmers/Programmers_86971.cpp
+++ Code/Programmers/Programmers_86971.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cmath>
 
 using namespace std;
 
@@ -32,16 +33,60 @@ int main()
 
 int BFS(int iNum, const vector<vector<int>>& vecWire)
 {
+	queue<int> qWire;
+	vector<bool> vecVisit(vecWire.size() + 1, false);
+	int iCount{};
 
+	qWire.push(iNum);
+	vecVisit[iNum] = true;
+	iCount++;
 
-	return 0;
+	while (!qWire.empty())
+	{
+		int iWire = qWire.front();
+		qWire.pop();
+
+		for (int i = 0; i < vecWire[iWire - 1].size(); i++)
+		{
+			if (vecVisit[vecWire[iWire - 1][i]] == false)
+			{
+				qWire.push(vecWire[iWire - 1][i]);
+				vecVisit[vecWire[iWire - 1][i]] = true;
+				iCount++;
+			}
+		}
+	}
+
+	return iCount;
 }
 
 int solution(int n, vector<vector<int>> wires)
 {
-	int answer = -1;
+	int answer = 100;
 
+	queue<int> qWire;
+	vector<bool> vecVisit(wires.size() + 1, false);
 
+	qWire.push(1);
+	vecVisit[1] = true;
+
+	while (!qWire.empty())
+	{
+		int iWire = qWire.front();
+		qWire.pop();
+
+		for (int i = 0; i < wires[iWire - 1].size(); i++)
+		{
+			if (vecVisit[wires[iWire - 1][i]] == false)
+			{
+				qWire.push(wires[iWire - 1][i]);
+				vecVisit[wires[iWire - 1][i]] = true;
+
+				int iBFS = abs(n - BFS(wires[iWire - 1][i], wires));
+				answer = answer > iBFS ? iBFS : answer;
+			}
+		}
+	}
 
 	return answer;
 }
