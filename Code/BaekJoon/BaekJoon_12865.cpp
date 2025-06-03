de93acc785a71e9d5d5d1c2aab7dfd414c33bab5
youns119 <youns119@naver.com>
2025-06-04 01:17:49 +0900

    asdf

--- Code/BaekJoon/BaekJoon_12865.cpp
+++ Code/BaekJoon/BaekJoon_12865.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -7,7 +9,25 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
+	int iCnt{}, iMaxWeight{};
 
+	cin >> iCnt >> iMaxWeight;
+	vector<int> vecWeight(iCnt);
+	vector<int> vecDP(iMaxWeight + 1);
+
+	for (int i = 0; i < iCnt; i++)
+	{
+		int iWeight{}, iValue{};
+
+		cin >> iWeight >> iValue;
+		vecWeight[i] = iWeight;
+		vecDP[iWeight] = iValue;
+	}
+
+	for (int i = 0; i < iCnt; i++)
+		vecDP[iMaxWeight] = max(vecDP[iMaxWeight], vecDP[vecWeight[i]] + vecDP[iMaxWeight - vecWeight[i]]);
+
+	cout << vecDP[iMaxWeight];
 
 	return 0;
 }
