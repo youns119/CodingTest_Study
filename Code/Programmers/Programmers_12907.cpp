16337270c50c8207d278920e5ebc05dbf6c1f758
youns119 <youns119@naver.com>
2025-06-05 02:24:59 +0900

    asdf

--- Code/Programmers/Programmers_12907.cpp
+++ Code/Programmers/Programmers_12907.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <functional>
 
 using namespace std;
 
@@ -25,9 +26,47 @@ int main()
 
 int solution(int n, vector<int> money) 
 {
-	int answer = 0;
+    int answer = 0;
 
+    vector<int> vecDP(n + 1, 0);
+    vecDP[0] = 1;
 
+    for (int iCoin : money)
+        for (int i = iCoin; i <= n; ++i)
+            vecDP[i] += vecDP[i - iCoin];
 
-	return answer;
+    return vecDP[n] % 1'000'000'007;
 }
+
+// 내가 처음에 푼 DFS 방식의 풀이
+// 테스트 케이스는 다 통과했는데 효율성에서 광탈함...
+// 아무래도 모든 경우의 수를 다 탐색하는 방법이다보니 당연한 듯
+
+#ifdef _RELEASE
+
+int solution(int n, vector<int> money)
+{
+    int answer = 0;
+
+    function<void(int, int)> DFS = [&](int iCurr, int iSum)
+        {
+            if (iSum >= n)
+            {
+                if (iSum == n) answer++;
+                return;
+            }
+
+            for (int i = 0; i < money.size(); i++)
+            {
+                if (iCurr > money[i]) continue;
+
+                DFS(money[i], iSum + money[i]);
+            }
+        };
+
+    DFS(0, 0);
+
+    return answer % 1'000'000'007;
+}
+
+#endif
