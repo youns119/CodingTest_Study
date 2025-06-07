475ebff0700c3c91fb106a7a6d9b00aa9a9e5b24
youns119 <youns119@naver.com>
2025-06-07 18:32:17 +0900

    ㅁㄴㅇㄹ

--- Code/Programmers/Programmers_12929.cpp
+++ Code/Programmers/Programmers_12929.cpp
@@ -41,3 +41,26 @@ int solution(int n)
 
 	return answer;
 }
+
+// 카탈란 수 + DP 방식으로 푸는 방법
+// 그냥 카탈란 수의 점화식 자체를 DP로 만든거라고 보면 된다
+// 이 카탈란 수라는건 조합론에서 상당히 자주 쓰인다고 한다... 기억하자!
+
+#ifdef _RELEASE
+
+int solution(int n) {
+	vector<long long> dp(n + 1);
+	dp[0] = 1;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
+			dp[i] += dp[j] * dp[i - 1 - j];
+		}
+	}
+
+	return dp[n];
+}
+
+#endif
