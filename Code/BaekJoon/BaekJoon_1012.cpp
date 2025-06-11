3b4796f75d1293c05a225296009e1763bf3a6264
youns119 <youns119@naver.com>
2025-06-11 18:37:49 +0900

    ㅁㄴㅇㄹ

--- Code/BaekJoon/BaekJoon_1012.cpp
+++ Code/BaekJoon/BaekJoon_1012.cpp
@@ -89,3 +89,71 @@ int BaeChu(vector<vector<int>>& vecField)
 
 	return iResult;
 }
+
+// BFS 대신 DFS로 푸는 풀이
+// 제한 조건이 그렇게 범위가 넓지 않으므로 DFS로도 충분히 스택 오버플로우 없이 가능
+// 방문 체크용 이중 vector를 하나 더 쓰는 것 말고는 딱히 구현 상의 차이점은 없다
+
+#ifdef _RELEASE
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int T, M, N, K;
+vector<vector<int>> field;
+vector<vector<bool>> visited;
+int dx[4] = { 0, 0, -1, 1 }; // 좌우상하
+int dy[4] = { -1, 1, 0, 0 };
+
+void dfs(int x, int y) {
+	visited[y][x] = true;
+
+	for (int dir = 0; dir < 4; dir++) {
+		int nx = x + dx[dir];
+		int ny = y + dy[dir];
+
+		if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
+			if (field[ny][nx] == 1 && !visited[ny][nx]) {
+				dfs(nx, ny);
+			}
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> T;
+
+	while (T--) {
+		cin >> M >> N >> K;
+
+		field.assign(N, vector<int>(M, 0));
+		visited.assign(N, vector<bool>(M, false));
+
+		for (int i = 0; i < K; i++) {
+			int x, y;
+			cin >> x >> y;
+			field[y][x] = 1;
+		}
+
+		int wormCount = 0;
+
+		for (int y = 0; y < N; y++) {
+			for (int x = 0; x < M; x++) {
+				if (field[y][x] == 1 && !visited[y][x]) {
+					dfs(x, y);
+					wormCount++;
+				}
+			}
+		}
+
+		cout << wormCount << '\n';
+	}
+
+	return 0;
+}
+
+#endif
