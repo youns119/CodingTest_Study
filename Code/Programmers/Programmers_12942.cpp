8e091861a350fe719d2f4178f303d763ec521685
youns119 <youns119@naver.com>
2025-06-12 21:09:17 +0900

    asdf

---
+++ Code/Programmers/Programmers_12942.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(vector<vector<int>> matrix_sizes);
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+
+
+	return 0;
+}
+
+int solution(vector<vector<int>> matrix_sizes)
+{
+	int answer = 0;
+
+	int iMax{};
+	int arr[201][201]{};
+
+	for (auto Matrix : matrix_sizes)
+	{
+		int iFirst = Matrix[0];
+		int iSecond = Matrix[1];
+
+		arr[iFirst]
+	}
+
+	return answer;
+}
