4a5b1778f7e3c96a762efb7d9bb793d1c7d66da3
youns119 <youns119@naver.com>
2025-06-16 03:25:16 +0900

    asdf

---
+++ Code/Programmers/Programmers_12927.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n, vector<int> works);
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	return 0;
+}
+
+long long solution(int n, vector<int> works) 
+{
+	long long answer = 0;
+
+
+
+	return answer;
+}
